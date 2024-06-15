a2e422ce9dd67ea20bf537b746d66225084deec3
Aryan <aryanmusicbht@gmail.com>
2024-06-15 17:58:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementsByClassName("btn")[0];
+
+button.addEventListener("click",() => {
+    window.open('index.html', '_blank');
+}
+)
+
+let ab = document.getElementsByClassName("ab")[0];
+
+ab.addEventListener("click",() => {
+    window.open('index.html', '_blank');
+}
+)
